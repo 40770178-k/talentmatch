d1858fe81ec3d25f9545d5de62488f9285962ed0
40770178-k <153083964+40770178-k@users.noreply.github.com>
2026-06-05 10:15:12 +0300

    Initial ATS setup with custom user model

---
+++ users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+
+    ROLE_CHOICES = (
+        ("candidate", "Candidate"),
+        ("recruiter", "Recruiter"),
+        ("admin", "Admin"),
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default="candidate"
+    )
