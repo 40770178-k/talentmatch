c3249070d1afb5ad43c582682b026a100f3dbe95
40770178-k <153083964+40770178-k@users.noreply.github.com>
2026-06-10 21:00:10 +0300

    created serializers.py in accounts

---
+++ accounts/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from users.models import CustomUser
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CustomUser
+        fields = [
+            "username",
+            "email",
+            "password",
+            "role",
+        ]
