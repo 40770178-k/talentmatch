645f6f7719b6407f40a06ff7292a90658fd57527
40770178-k <153083964+40770178-k@users.noreply.github.com>
2026-06-12 20:48:33 +0300

    updated serializer to not return passwords to client

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -13,12 +13,18 @@ class RegistrationSerializer(serializers.ModelSerializer):
             "role",
         ]
 
-    def create(self, validated_data):
-     user = CustomUser.objects.create_user(
-        username=validated_data["username"],
-        email=validated_data["email"],
-        password=validated_data["password"],
-        role=validated_data["role"],
-    )
+        extra_kwargs = {
+            "password": {
+                "write_only": True
+            }
+        }
 
-     return user
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+            role=validated_data["role"],
+        )
+
+        return user
