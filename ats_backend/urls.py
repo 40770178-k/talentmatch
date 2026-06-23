a45f0b802fa9a8072aa6a3e9df0c95e8f33b17f7
40770178-k <153083964+40770178-k@users.noreply.github.com>
2026-06-23 08:59:50 +0300

    added file handling urls

--- ats_backend/urls.py
+++ ats_backend/urls.py
@@ -24,3 +24,8 @@ urlpatterns = [
     path("api/jobs/", include("jobs.urls")),
     path("api/applications/", include("applications.urls")),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
