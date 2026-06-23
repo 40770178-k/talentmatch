0a12a5b4e920ae741182edf8cde2b5593ff39527
40770178-k <153083964+40770178-k@users.noreply.github.com>
2026-06-23 09:00:49 +0300

    created model for resumes

---
+++ resumes/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from users.models import CustomUser
+
+
+class Resume(models.Model):
+    candidate = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name="resumes"
+    )
+    file = models.FileField(upload_to="resumes/")
+    original_filename = models.CharField(max_length=255, blank=True)
+    is_active = models.BooleanField(default=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.candidate.username} - {self.original_filename}"
