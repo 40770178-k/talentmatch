971636478b84ec9cd9866e49a5d00e958dca300e
40770178-k <153083964+40770178-k@users.noreply.github.com>
2026-07-11 19:28:29 +0300

    added url for dashboard edpoint

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import JobListCreateView, JobDetailView
+from .views import JobListCreateView, JobDetailView, RecruiterDashboardView
 
 urlpatterns = [
     path("", JobListCreateView.as_view(), name="job-list"),
     path("<int:pk>/", JobDetailView.as_view(), name="job-detail"),
+    path(
+        "dashboard/",
+        RecruiterDashboardView.as_view(),
+        name="recruiter-dashboard"
+    ),
 ]
