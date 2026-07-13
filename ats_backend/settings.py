de724b0f141e5367d4fd988f3ff39ebe76eaa0ef
40770178-k <153083964+40770178-k@users.noreply.github.com>
2026-07-13 09:59:47 +0300

    setup react

--- ats_backend/settings.py
+++ ats_backend/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'resumes',
     'applications',
     'jobs',
@@ -54,6 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -158,3 +160,7 @@ CELERY_RESULT_SERIALIZER = "json"
 
 
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
