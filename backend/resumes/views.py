f161f04d4c8bffc90fda6847f0898e523460a753
40770178-k <153083964+40770178-k@users.noreply.github.com>
2026-07-31 09:03:04 +0300

    addedvalidatio to file uploads

--- backend/resumes/views.py
+++ backend/resumes/views.py
@@ -4,6 +4,7 @@ from users.permissions import IsCandidate
 from .models import Resume
 from .serializers import ResumeSerializer
 from .tasks import parse_resume
+from rest_framework.exceptions import ValidationError
 
 class ResumeListCreateView(generics.ListCreateAPIView):
     serializer_class = ResumeSerializer
@@ -32,3 +33,35 @@ class ResumeListCreateView(generics.ListCreateAPIView):
         )
 
         parse_resume.delay(resume.id)
+
+ALLOWED_EXTENSIONS = [".pdf", ".docx"]
+MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
+
+def perform_create(self, serializer):
+    file = self.request.data.get("file")
+
+    # Check file extension
+    import os
+    ext = os.path.splitext(file.name)[1].lower()
+    if ext not in ALLOWED_EXTENSIONS:
+        raise ValidationError(
+            "Only PDF and DOCX files are allowed."
+        )
+
+    # Check file size
+    if file.size > MAX_FILE_SIZE:
+        raise ValidationError(
+            "File size cannot exceed 5MB."
+        )
+
+    Resume.objects.filter(
+        candidate=self.request.user,
+        is_active=True
+    ).update(is_active=False)
+
+    resume = serializer.save(
+        candidate=self.request.user,
+        original_filename=file.name,
+    )
+
+    parse_resume.delay(resume.id)
